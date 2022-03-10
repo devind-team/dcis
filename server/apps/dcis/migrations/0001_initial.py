cfec15573795862f6ddb05a1d6e12f4d7eda24cc
squak <squakrazv@gmail.com>
2022-03-10 23:06:40 +0300

    fix: правка миграции

--- server/apps/dcis/migrations/0001_initial.py
+++ server/apps/dcis/migrations/0001_initial.py
@@ -283,7 +283,7 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='document',
-            name='sheet',
+            name='sheets',
             field=models.ManyToManyField(to='dcis.Sheet'),
         ),
         migrations.CreateModel(
