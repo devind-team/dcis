14fd72ec1f596e6c3dd0f3824fa7352cd784ffb2
Victor <lyferov@yandex.ru>
2022-03-17 22:56:16 +0300

    feat: Адаптация изменения цветовой темы в зависимости от ширины монитора

--- client/components/global/ThemeColor.vue
+++ client/components/global/ThemeColor.vue
@@ -12,28 +12,26 @@
           v-col(cols="12")
             v-btn-toggle(v-model="$colorMode.preference" tile mandatory)
               v-btn(v-for="theme in themes" :key="theme.value" :value="theme.value")
-                | {{ $t(`index.theme.${theme.value}`) }}
-                v-icon(right) mdi-{{ theme.icon }}
+                span(v-show="$vuetify.breakpoint.smAndUp" ) {{ $t(`index.theme.${theme.value}`) }}
+                v-icon(:right="$vuetify.breakpoint.smAndUp") mdi-{{ theme.icon }}
           v-col(cols="12") {{ $t('index.theme.selected', { theme: $t(`index.theme.${$colorMode.preference}`) }) }}
         v-row.text-center(v-if="$colorMode.preference === 'system'")
           v-col {{ $t('index.theme.detected', { theme: $t(`index.theme.${$colorMode.value}`) }) }}
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import type { Ref } from '#app'
+import { defineComponent, ref } from '#app'
 
-export default Vue.extend<any, any, any, any>({
-  data: () => ({
-    active: false
-  }),
-  computed: {
-    themes (): { value: string, icon: string }[] {
-      return [
-        { value: 'system', icon: 'desktop-classic' },
-        { value: 'light', icon: 'lightbulb-on-outline' },
-        { value: 'dark', icon: 'lightbulb-on' }
-      ]
-    }
+export default defineComponent({
+  setup () {
+    const active: Ref<boolean> = ref<boolean>(false)
+    const themes: { value: string, icon: string }[] = [
+      { value: 'system', icon: 'desktop-classic' },
+      { value: 'light', icon: 'lightbulb-on-outline' },
+      { value: 'dark', icon: 'lightbulb-on' }
+    ]
+    return { active, themes }
   }
 })
 </script>
