93fb02a27ce057945fd02c8a6dcbe3e7e166fc72
Victor <lyferov@yandex.ru>
2022-03-18 00:26:41 +0300

    feat: Рефакторинг блока подвала

--- client/components/global/FooterComponent.vue
+++ client/components/global/FooterComponent.vue
@@ -16,39 +16,36 @@
       v-row
         v-col(cols="12" md="9")
           .body-2.gray-text &copy; {{ $t('index.rights') }}. {{ new Date().getFullYear() }}
-    v-btn(v-show="upVisible$" @click="$vuetify.goTo(0)" size="small" fab fixed bottom right color="primary")
+    v-btn(v-show="upVisible" @click="$vuetify.goTo(0)" size="small" fab fixed bottom right color="primary")
       v-icon mdi-chevron-up
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { fromEvent } from 'rxjs'
-import { pluck, map, debounceTime, startWith } from 'rxjs/operators'
+import { useScroll } from '@vueuse/core'
+import type { Ref } from '#app'
+import { defineComponent, ref, onMounted, watchEffect } from '#app'
 import ThemeColor from '~/components/global/ThemeColor.vue'
 
-export default Vue.extend<any, any, any, any>({
+export default defineComponent({
   components: { ThemeColor },
-  data: () => ({
-    upVisible$: false,
-    counterSrc: ''
-  }),
-  subscriptions () {
-    const upVisible$ = fromEvent(document, 'scroll').pipe(
-      pluck('target', 'documentElement'),
-      debounceTime(100),
-      map((target: any) => ({ top: target.scrollTop + window.innerHeight, height: target.offsetHeight })),
-      map(({ top, height }) => (top + 800 > height)),
-      startWith(false),
-      map(() => window.scrollY > 20)
-    )
-    return { upVisible$ }
-  },
-  mounted () {
-    const s = screen.colorDepth ? screen.colorDepth : screen.pixelDepth
-    this.counterSrc = 'https://counter.yadro.ru/hit?t11.6;r' + encodeURIComponent(document.referrer) +
+  setup () {
+    const counterSrc: Ref<string | null> = ref<string | null>(null)
+    const upVisible: Ref<boolean> = ref<boolean>(false)
+
+    onMounted(() => {
+      const { y } = useScroll(document, { throttle: 500 })
+      watchEffect(() => {
+        upVisible.value = y.value > 200
+      })
+
+      const s: number = screen.colorDepth ? screen.colorDepth : screen.pixelDepth
+      counterSrc.value = 'https://counter.yadro.ru/hit?t11.6;r' + encodeURIComponent(document.referrer) +
       (typeof screen === 'undefined' ? '' : ';s' + screen.width + '*' + screen.height + '*' + s) +
       ';u' + encodeURIComponent(document.URL) + ';h' +
       encodeURIComponent(document.title.substring(0, 150)) + ';' + Math.random() as string
+    })
+
+    return { counterSrc, upVisible }
   }
 })
 </script>
