3b0773a2aaeaf7c1266c24861d76c2ebbcda2e86
Victor <lyferov@yandex.ru>
2022-03-21 15:41:34 +0300

    fix: Убрано ошибочное объединение в случае, когда добавление осуществляется к нижней объединенной ячейке

--- server/apps/dcis/models/document.py
+++ server/apps/dcis/models/document.py
@@ -43,7 +43,7 @@ class Sheet(models.Model):
         for merge_cells in self.mergedcell_set.all():
             if merge_cells.min_row <= idx <= merge_cells.max_row:
                 merge_cells.max_row += offset
-                if position == 'before' and merge_cells.min_row == idx:
+                if merge_cells.min_row == idx:
                     merge_cells.min_row += offset
             elif merge_cells.min_row > idx:
                 merge_cells.min_row += offset
