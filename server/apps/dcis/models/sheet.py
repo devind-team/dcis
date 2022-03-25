de9b6a2909199e736c13f1dd91deee02e161c33b
Victor <lyferov@yandex.ru>
2022-03-25 13:08:39 +0300

    feat: Обрана проверка на ограничение в БД

--- server/apps/dcis/models/sheet.py
+++ server/apps/dcis/models/sheet.py
@@ -297,7 +297,7 @@ class MergedCell(models.Model):
     sheet = models.ForeignKey(Sheet, on_delete=models.CASCADE, help_text='Лист')
 
     class Meta:
-        unique_together = [['min_col', 'min_row', 'sheet']]
+        ordering = ('min_col', 'min_row',)
 
     def __str__(self) -> str:
         return f'{get_column_letter(self.min_col)}{self.min_row}:' \
