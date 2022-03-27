833c6414b2c5cb94426f252530483ea8d7ada3da
Luferov Victor <lyferov@yandex.ru>
2022-03-27 23:22:30 +0300

    ref: Рефактор некоторых компонентов связанных с CMS

--- client/pages/categories/index.vue
+++ client/pages/categories/index.vue
@@ -10,23 +10,24 @@
                 v-btn(
                   :to="localePath({ name: 'categories-categoryId', params: { categoryId: category.id } })"
                   text
-                  ) {{ category.text }}
-                template(
-                  v-if="hasPerm(['pages.add_category', 'pages.change_category', 'pages.delete_category'], true)"
-                  )
+                ) {{ category.text }}
+                template(v-if="hasPerm(['pages.add_category', 'pages.change_category', 'pages.delete_category'], true)")
                   v-spacer
                   category-action(
                     :category="category"
-                    :addCategoryUpdate="addCategory"
-                    :changeCategoryUpdate="changeCategory"
-                    :deleteCategoryUpdate="deleteCategory" add)
+                    :addCategoryUpdate="addCategoryUpdate"
+                    :changeCategoryUpdate="changeCategoryUpdate"
+                    :deleteCategoryUpdate="deleteCategoryUpdate"
+                    add
+                  )
               v-card-text
                 v-list
                   v-list-item(
                     v-for="cat in category.children"
                     :key="cat.id"
                     :to="localePath({ name: 'categories-categoryId', params: { categoryId: cat.id } })"
-                    exact)
+                    exact
+                  )
                     v-list-item-avatar(v-if="cat.avatar")
                       v-img(:src="`/${cat.avatar}`")
                     v-list-item-content
@@ -34,118 +35,111 @@
                     v-list-item-action(v-if="hasPerm(['pages.change_category', 'pages.delete_category'], true)")
                       category-action(
                         :category="cat"
-                        :changeCategoryUpdate="changeCategory"
-                        :deleteCategoryUpdate="deleteCategory"
+                        :changeCategoryUpdate="changeCategoryUpdate"
+                        :deleteCategoryUpdate="deleteCategoryUpdate"
                         icon="mdi-dots-vertical")
           v-col(v-if="hasPerm('pages.add_category')" key="addCategory" cols="12" sm="6" md="4" lg="4")
-            add-category(:update="addCategory")
+            add-category(:update="addCategoryUpdate")
       template(#no-data)
         v-row
           v-col(v-if="hasPerm('pages.add_category')")
-            add-category(:update="addCategory")
+            add-category(:update="addCategoryUpdate")
           v-col(v-else) {{ $t('pages.category.noCategories') }}
 </template>
 
 <script lang="ts">
-import Vue, { AsyncComponent } from 'vue'
-import { Component } from 'vue-property-decorator'
-import { MetaInfo } from 'vue-meta'
-import { mapGetters } from 'vuex'
 import { DataProxy } from 'apollo-cache'
-import categoriesQuery from '~/gql/pages/queries/categories.graphql'
+import { computed, ComputedRef, defineComponent, toRefs, useNuxt2Meta } from '#app'
+import { HasPermissionFnType, useAuthStore } from '~/store'
+import { useI18n, useQueryRelay } from '~/composables'
 import { BreadCrumbsItem } from '~/types/devind'
 import {
   AddCategoryMutationPayload,
-  CategoryType,
-  CategoryTypeEdge
+  CategoriesQuery,
+  CategoriesQueryVariables,
+  CategoryType, CategoryTypeEdge,
+  ChangeCategoryMutationPayload,
+  DeleteCategoryMutationPayload
 } from '~/types/graphql'
+import categoriesQuery from '~/gql/pages/queries/categories.graphql'
 import BreadCrumbs from '~/components/common/BreadCrumbs.vue'
-const CategoryAction: AsyncComponent = () => import('~/components/pages/CategoryAction.vue')
-const AddCategory: AsyncComponent = () => import('~/components/pages/AddCategory.vue')
+import CategoryAction from '~/components/pages/CategoryAction.vue'
+import AddCategory from '~/components/pages/AddCategory.vue'
 
-@Component({
-  components: { BreadCrumbs, AddCategory, CategoryAction },
-  computed: {
-    ...mapGetters({ loginIn: 'auth/loginIn', hasPerm: 'auth/hasPerm' }),
-    bc (): BreadCrumbsItem[] {
-      return [
-        { text: this.$t('index.main') as string, to: this.localePath({ name: 'index' }), exact: true },
-        { text: this.$t('pages.categories') as string, to: this.localePath({ name: 'categories' }), exact: true }
-      ]
-    }
-  },
-  apollo: {
-    categories: {
-      query: categoriesQuery,
-      variables: { isNull: true },
-      update ({ categories }: any): CategoryType[] {
-        return categories.edges.map((e: any) => e.node)
+export type AddCategoryMutationResult = { data: { addCategory: AddCategoryMutationPayload } }
+export type ChangeCategoryMutationResult = { data: { changeCategory: ChangeCategoryMutationPayload } }
+export type DeleteCategoryMutationResult = { data: { deleteCategory: DeleteCategoryMutationPayload } }
+
+export default defineComponent({
+  components: { BreadCrumbs, CategoryAction, AddCategory },
+  setup () {
+    const authStore = useAuthStore()
+    const { t, localePath } = useI18n()
+    const { loginIn, hasPerm } = toRefs<{ loginIn: boolean, hasPerm: HasPermissionFnType }>(authStore)
+
+    useNuxt2Meta({ title: t('pages.categories') as string })
+
+    const { data: categories, loading, update } = useQueryRelay<CategoriesQuery, CategoriesQueryVariables, CategoryType>({
+      document: categoriesQuery,
+      variables: { isNull: true }
+    })
+
+    const bc: ComputedRef<BreadCrumbsItem[]> = computed<BreadCrumbsItem[]>(() => ([
+      { text: t('index.main') as string, to: localePath({ name: 'index' }), exact: true },
+      { text: t('pages.categories') as string, to: localePath({ name: 'categories' }), exact: true }
+    ]))
+
+    const addCategoryUpdate = (cache: DataProxy, result: AddCategoryMutationResult) => {
+      const { success } = result.data.addCategory
+      if (success) {
+        update(cache, result, (dataCache, { data: { addCategory: { category } } }) => {
+          category.parent
+            ? dataCache.categories
+              .edges.find((e: CategoryTypeEdge | any) => e.node.id === category.parent.id).node
+              .children.push(category as any)
+            : dataCache.categories.edges.push({ node: category, __typename: 'CategoryTypeEdge' } as any)
+          return dataCache
+        })
       }
     }
-  },
-  head (): MetaInfo {
-    return { title: this.$t('pages.categories') as string } as MetaInfo
+
+    const changeCategoryUpdate = (cache: DataProxy, result: ChangeCategoryMutationResult) => {
+      const { success } = result.data.changeCategory
+      if (success) {
+        update(cache, result, (dataCache, { data: { changeCategory: { category } } }) => {
+          dataCache.categories.edges = dataCache.categories.edges.map((e: CategoryTypeEdge | any) => {
+            if (e.node?.id === category.id) {
+              e.node = Object.assign(category, e.node!)
+            }
+            e.node!.children = e.node!.children.map((element: any) => {
+              if (element!.id === category.id) {
+                return Object.assign(category, element!)
+              }
+              return element
+            })
+            return e
+          })
+          return dataCache
+        })
+      }
+    }
+
+    const deleteCategoryUpdate = (cache: DataProxy, result: DeleteCategoryMutationResult, category: CategoryType) => {
+      const { success } = result.data.deleteCategory
+      if (success) {
+        update(cache, result, (dataCache, _) => {
+          dataCache.categories.edges = dataCache.categories.edges
+            .filter((e: CategoryTypeEdge | any) => e.node.id !== category.id)
+            .map((e: CategoryTypeEdge | any) => {
+              e.node.children = e.node.children.filter((e: any) => e.id !== category.id)
+              return e
+            })
+          return dataCache
+        })
+      }
+    }
+
+    return { bc, loginIn, hasPerm, categories, loading, addCategoryUpdate, changeCategoryUpdate, deleteCategoryUpdate }
   }
 })
-export default class CategoryIndex extends Vue {
-  categories!: CategoryType[]
-  loginIn!: boolean
-  hasPerm!: (permissions: string | string[], or?: boolean) => boolean
-
-  /**
-   * Добавляем новую категорию
-   */
-  addCategory (
-    store: DataProxy,
-    { data: { addCategory: { success, category } } }: { data: { addCategory: AddCategoryMutationPayload } }
-  ) {
-    if (success) {
-      const data: any = store.readQuery({ query: categoriesQuery, variables: { isNull: true } })
-      if (category?.parent) {
-        data.categories.edges.find((e: CategoryTypeEdge) => e.node!.id === category?.parent!.id).node.children.push(category)
-      } else {
-        data.categories.edges.push({ node: category, __typename: 'CategoryTypeEdge' })
-      }
-      store.writeQuery({ query: categoriesQuery, variables: { isNull: true }, data })
-    }
-  }
-
-  changeCategory (store: DataProxy, { data: { changeCategory: { success, category } } }: any) {
-    if (success) {
-      const data: any = store.readQuery({ query: categoriesQuery, variables: { isNull: true } })
-      data.categories.edges = data.categories.edges.map((e: CategoryTypeEdge) => {
-        if (e.node?.id === category.id) {
-          e.node = Object.assign(category, e.node!)
-        }
-        e.node!.children = e.node!.children.map((element: any) => {
-          if (element!.id === category.id) {
-            return Object.assign(category, element!)
-          }
-          return element
-        })
-        return e
-      })
-      store.writeQuery({ query: categoriesQuery, variables: { isNull: true }, data })
-    }
-  }
-
-  /**
-   * Удаляем категорию
-   * @param store
-   * @param success
-   * @param category
-   */
-  deleteCategory (store: DataProxy, { data: { deleteCategory: { success } } }: any, category: CategoryType) {
-    if (success) {
-      const data: any = store.readQuery({ query: categoriesQuery, variables: { isNull: true } })
-      data.categories.edges = data.categories.edges
-        .filter((e: CategoryTypeEdge) => e.node?.id !== category.id)
-        .map((e: CategoryTypeEdge) => {
-          e.node!.children = e.node!.children.filter((e: any) => e!.id !== category.id)
-          return e
-        })
-      store.writeQuery({ query: categoriesQuery, variables: { isNull: true }, data })
-    }
-  }
-}
 </script>
