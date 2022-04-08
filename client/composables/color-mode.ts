57b33e0685d8f0ac3c72bc76dcfbb6c4c4c66923
Victor <lyferov@yandex.ru>
2022-04-08 16:15:03 +0300

    fix: Проблема с изменением темы светлой/темной

--- client/composables/color-mode.ts
+++ client/composables/color-mode.ts
@@ -1,13 +1,15 @@
 import { VueConstructor } from 'vue'
-import { getCurrentInstance } from '#app'
+import { computed, getCurrentInstance } from '#app'
 
 export function useVuetify () {
   const instance = getCurrentInstance()
   const vm = instance?.proxy || instance as unknown as InstanceType<VueConstructor>
 
+  const isDark = computed<boolean>(() => (vm.$colorMode.value === 'dark'))
+
   return {
     vuetify: vm.$vuetify,
     colorMode: vm.$colorMode,
-    isDark: vm.$colorMode.value === 'dark'
+    isDark
   }
 }
