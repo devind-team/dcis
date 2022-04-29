b267613ad4bdfef2ba5e78349f0a0d7d2bd0bf8a
Victor <lyferov@yandex.ru>
2022-04-29 12:36:44 +0300

    feat: Добавление серверной мутации

--- server/apps/dcis/schema/mutations/__init__.py
+++ server/apps/dcis/schema/mutations/__init__.py
@@ -1,3 +1,4 @@
+from .cell_mutations import CellMutations
 from .document_mutations import DocumentMutations
 from .project_mutations import ProjectMutations
 from .sheet_mutations import SheetMutations
