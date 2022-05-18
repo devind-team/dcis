ec832e7f44c290a84ce98c539e17a6fcabf505c6
squak <squakrazv@gmail.com>
2022-05-18 16:54:27 +0300

    fix: поправлено добавление класса на вышележащий элемент

--- client/composables/overlying-class.ts
+++ client/composables/overlying-class.ts
@@ -7,17 +7,21 @@ import { Ref } from '#app'
  */
 export function useOverlyingClass (element: HTMLElement, className: Ref<string | string[] | null>) {
   const removeClass = (value: string | string[] | null) => {
-    if (Array.isArray(value)) {
-      element.classList.remove(...value)
-    } else if (className) {
-      element.classList.remove(value)
+    if (value) {
+      if (Array.isArray(value)) {
+        element.classList.remove(...value)
+      } else if (className) {
+        element.classList.remove(value)
+      }
     }
   }
   const addClass = (value: string | string[] | null) => {
-    if (Array.isArray(value)) {
-      element.classList.add(...value)
-    } else if (className) {
-      element.classList.add(value)
+    if (value) {
+      if (Array.isArray(value)) {
+        element.classList.add(...value)
+      } else if (className) {
+        element.classList.add(value)
+      }
     }
   }
   watch(className, (newValue, oldValue) => {
