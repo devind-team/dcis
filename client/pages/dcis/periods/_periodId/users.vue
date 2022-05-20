c3c5dda486307522e3f148d47e89e90fd4a97728
Luferov Victor <lyferov@yandex.ru>
2022-05-20 14:08:19 +0300

    ref: Элементы управления вынесены как явные карточки

--- client/pages/dcis/periods/_periodId/users.vue
+++ client/pages/dcis/periods/_periodId/users.vue
@@ -5,21 +5,6 @@
         v-col(cols="12" md="4" sm="4")
           v-row
             v-subheader Группы
-            v-spacer
-            add-period-group(:period="period" :update="addPeriodGroupUpdate")
-              template(#activator="{ on: onMenu }")
-                v-tooltip(bottom)
-                  template(#activator="{ on: onTooltip }")
-                    v-btn(v-on="{...onMenu, ...onTooltip}" class="align-self-center mr-4" color="primary" icon)
-                      v-icon(large) mdi-plus-circle-outline
-                  span {{ $t('dcis.periods.actions.addGroup') }}
-            copy-period-groups(:period="period" active-query)
-              template(#activator="{ on: onMenu }")
-                v-tooltip(bottom)
-                  template(#activator="{ on: onTooltip }")
-                    v-btn(v-on="{...onMenu, ...onTooltip}" class="align-self-center mr-4" color="primary" icon)
-                      v-icon(large) mdi-import
-                  span {{ $t('dcis.periods.actions.copyGroups') }}
           v-list
             v-list-item-group(v-model="selectGroup" color="primary")
               v-list-item(
@@ -39,7 +24,20 @@
                         span {{ $t('dcis.periods.actions.deleteGroup') }}
         v-divider(vertical)
         v-col(cols="12" md="8" sm="6")
-          period-group-users(:period-group="selectGroup" :period="period" :update="deleteUserFromPeriodGroupUpdate")
+          period-group-users(v-if="selectGroup" :period-group="selectGroup" :period="period" :update="deleteUserFromPeriodGroupUpdate")
+          v-row(v-else)
+            v-col
+              add-period-group(:period="period" :update="addPeriodGroupUpdate")
+                template(#activator="{ on }")
+                  v-card.period-users-card-add(v-on="on" flat)
+                    v-icon(large) mdi-plus-circle-outline
+                    .title {{ $t('dcis.periods.actions.addGroup') }}
+            v-col
+              copy-period-groups(:period="period" active-query)
+                template(#activator="{ on }")
+                  v-card.period-users-card-add(v-on="on" flat)
+                    v-icon(large) mdi-import
+                    .title {{ $t('dcis.periods.actions.copyGroups') }}
 </template>
 
 <script lang="ts">
@@ -54,7 +52,7 @@ import LeftNavigatorContainer from '~/components/common/grid/LeftNavigatorContai
 import PeriodGroupUsers, { DeleteUserFromPeriodGroupMutationResult } from '~/components/dcis/periods/PeriodGroupUsers.vue'
 import DeleteMenu from '~/components/common/menu/DeleteMenu.vue'
 import AddPeriodGroup, { AddPeriodGroupMutationResult } from '~/components/dcis/periods/AddPeriodGroup.vue'
-import CopyPeriodGroups, {CopyPeriodGroupsMutationResult} from '~/components/dcis/periods/CopyPeriodGroups.vue'
+import CopyPeriodGroups, { CopyPeriodGroupsMutationResult } from '~/components/dcis/periods/CopyPeriodGroups.vue'
 import { ChangePeriodGroupUsersMutationResult } from '~/components/dcis/periods/AddPeriodGroupUsers.vue'
 import { ChangePeriodGroupPrivilegesMutationResult } from '~~/components/dcis/periods/PeriodGroupPrivileges.vue'
 
@@ -78,7 +76,11 @@ export default defineComponent({
     const selectGroup = ref<PeriodGroupType | null | undefined>(null)
     const periodUpdate: any = inject('periodUpdate')
 
-    // Обновление после добавления группы
+    /**
+     *  Обновление после добавления группы
+     * @param cache
+     * @param result
+     */
     const addPeriodGroupUpdate = (cache: DataProxy, result: AddPeriodGroupMutationResult) => {
       periodUpdate(cache, result, (dataCache, { data: { addPeriodGroup: { errors, periodGroup } } }: AddPeriodGroupMutationResult) => {
         if (!errors.length) {
@@ -88,7 +90,11 @@ export default defineComponent({
       })
     }
 
-    // Обновление после добавления пользователей
+    /**
+     * Обновление после добавления пользователей
+     * @param cache
+     * @param result
+     */
     const changePeriodGroupUsersUpdate = (cache: DataProxy, result: ChangePeriodGroupUsersMutationResult) => {
       periodUpdate(
         cache, result, (dataCache, { data: { changePeriodGroupUsers: { errors, users } } }: ChangePeriodGroupUsersMutationResult
@@ -100,7 +106,11 @@ export default defineComponent({
         })
     }
 
-    // Обновление после добавления групп из другого сбора
+    /**
+     *  Обновление после добавления групп из другого сбора
+     * @param cache
+     * @param result
+     */
     const copyPeriodGroupsUpdate = (cache: DataProxy, result: CopyPeriodGroupsMutationResult) => {
       periodUpdate(
         cache, result, (dataCache, { data: { copyPeriodGroups: { success, periodGroups } } }: CopyPeriodGroupsMutationResult
@@ -112,7 +122,11 @@ export default defineComponent({
         })
     }
 
-    // Обновление после изменения привилегий группы
+    /**
+     * Обновление после изменения привилегий группы
+     * @param cache
+     * @param result
+     */
     const changePeriodGroupPrivilegesUpdate = (cache: DataProxy, result: ChangePeriodGroupPrivilegesMutationResult) => {
       periodUpdate(
         cache, result, (dataCache, { data: { changePeriodGroupPrivileges: { errors, privileges } } }: ChangePeriodGroupPrivilegesMutationResult
@@ -125,7 +139,9 @@ export default defineComponent({
         })
     }
 
-    // Обновление после удаления группы
+    /**
+     * Обновление после удаления группы
+     */
     const { mutate: deletePeriodGroupMutate } = useMutation<DeletePeriodGroupMutation, DeletePeriodGroupMutationVariables>(deletePeriodGroup, {
       update: (cache, result) => periodUpdate(
         cache,
@@ -140,7 +156,11 @@ export default defineComponent({
       )
     })
 
-    // Обновление после удаления пользователя из группы
+    /**
+     * Обновление после удаления пользователя из группы
+     * @param cache
+     * @param result
+     */
     const deleteUserFromPeriodGroupUpdate = (cache: DataProxy, result: DeleteUserFromPeriodGroupMutationResult) => {
       periodUpdate(
         cache, result, (dataCache, { data: { deleteUserFromPeriodGroup: { errors, id } } }: DeleteUserFromPeriodGroupMutationResult
@@ -169,3 +189,12 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="sass">
+.period-users-card-add
+  padding: 20px 0
+  border: 1px dashed lightgray !important
+  display: flex
+  text-align: center
+  flex-flow: column
+</style>
