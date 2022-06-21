ce578f2e8eb7e57b2be6bb8a446e90c3bbef7678
squak <squakrazv@gmail.com>
2022-06-21 15:24:07 +0300

    feat: сделаны разрешения для листа

---
+++ server/apps/dcis/permissions/__init__.py
@@ -0,0 +1,1 @@
+from .sheet_permissions import AddChildRowDimension, ChangeSheet, ChangeValue, DeleteRowDimension, ViewDocument
--- server/apps/dcis/services/divisions_services.py
+++ server/apps/dcis/services/divisions_services.py
@@ -3,10 +3,11 @@
 from django.db.models import Q
 
 from apps.core.models import User
-from apps.dcis.models import Project
+from apps.dcis.models import Document, Project
 
 
 def get_user_divisions(user: User, project: Project | str | None = None) -> list[dict[str, str]]:
+    """Получение списка обобщенных дивизионов для пользователя user и проекта project."""
     if project is None:
         divisions: list[dict] = []
         for division_model in Project.DIVISION_KIND.values():
@@ -17,7 +18,14 @@ def get_user_divisions(user: User, project: Project | str | None = None) -> list
     return _get_division(project.division.objects.filter(Q(users=user) | Q(user=user)))
 
 
-def _get_division(instances) -> list[dict]:
+def document_in_user_divisions(document: Document, user: User) -> bool:
+    """Принадлежит ли документ одному из дивизионов пользователя."""
+    return document.object_id in [
+        division['id'] for division in get_user_divisions(user, document.period.project)
+    ]
+
+
+def _get_division(instances) -> list[dict[str, str]]:
     """Получение списка обобщенных дивизионов."""
     return [{
         'id': division.id,
