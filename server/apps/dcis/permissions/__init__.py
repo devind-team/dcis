3dbbd16f5bf99ef32b9d1b771a9863efbb8dd786
Victor <lyferov@yandex.ru>
2022-06-22 12:00:58 +0300

    feat: Пермишенны разнесены по файлам

--- server/apps/dcis/permissions/__init__.py
+++ server/apps/dcis/permissions/__init__.py
@@ -1,1 +1,4 @@
 from .sheet_permissions import AddChildRowDimension, ChangeSheet, ChangeValue, DeleteRowDimension, ViewDocument
+from .project_permissions import AddProject, ChangeProject, DeleteProject
+from .period_permissions import AddPeriod, ChangePeriod, DeletePeriod
+from .document_permissions import AddDocument, ChangeDocument, DeleteDocument, AddDocumentStatus, DeleteDocumentStatus
---
+++ server/apps/dcis/permissions/project_permissions.py
@@ -0,0 +1,5 @@
+from devind_helpers.permissions import ModelPermission
+
+AddProject = ModelPermission('dcis.add_project')
+ChangeProject = ModelPermission('dcis.change_project')
+DeleteProject = ModelPermission('dcis.delete_project')
