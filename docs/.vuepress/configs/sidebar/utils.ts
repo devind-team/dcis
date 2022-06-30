980ca53a997b714e5fb409bf06c1fb0ceefccc73
MarionetkaX <sergey07061999@gmail.com>
2022-06-30 16:14:40 +0300

    ref: add collapsible in sidebar

--- docs/.vuepress/configs/sidebar/utils.ts
+++ docs/.vuepress/configs/sidebar/utils.ts
@@ -3,7 +3,7 @@ import { SidebarConfigArray } from '@vuepress/theme-default'
 const fs = require('fs')
 const path = require('path')
 
-export function getSideBar(dir: string, root: string): SidebarConfigArray {
+export function getSideBar(dir: string, root: string, level: number = 1): SidebarConfigArray {
     const sideBarArray: SidebarConfigArray = []
     for (const entry of fs.readdirSync(dir)) {
         const entryPath = path.join(dir, entry)
@@ -11,7 +11,8 @@ export function getSideBar(dir: string, root: string): SidebarConfigArray {
         if (stats.isDirectory()) {
             sideBarArray.push({
                 text: entry,
-                children: getSideBar(entryPath, root)
+                collapsible: ((level > 1) ? true : false),
+                children: getSideBar(entryPath, root, level + 1)
             })
         } else if (stats.isFile()) {
             if (!entry.match(/readme\.md/i)) {
