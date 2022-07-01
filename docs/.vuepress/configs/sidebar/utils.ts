f0392e3605a04128f240c1ec5b13671577ab8cbb
squak <squakrazv@gmail.com>
2022-07-01 14:28:41 +0300

    feat: изменен внешний вид сайдбара

--- docs/.vuepress/configs/sidebar/utils.ts
+++ docs/.vuepress/configs/sidebar/utils.ts
@@ -11,15 +11,16 @@ export function getSideBar(dir: string, root: string, level: number = 1): Sideba
         if (stats.isDirectory()) {
             sideBarArray.push({
                 text: entry,
-                collapsible: ((level > 1) ? true : false),
+                collapsible: level <= 2,
                 children: getSideBar(entryPath, root, level + 1)
             })
         } else if (stats.isFile()) {
             if (!entry.match(/readme\.md/i)) {
                 const splitEntry = entryPath.split(path.sep)
-                sideBarArray.push(
-                    `/${splitEntry.slice(splitEntry.lastIndexOf(root)).join('/')}`
-                )
+                sideBarArray.push({
+                    text: path.basename(entry, '.md'),
+                    link: `/${splitEntry.slice(splitEntry.lastIndexOf(root)).join('/')}`
+                })
             }
         }
     }
