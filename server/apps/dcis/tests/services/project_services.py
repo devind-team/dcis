edcddcbc6d67c8cfb4c63ebdf07f68e3444a4aa9
MiloslavskiySergey <sergey07061999@gmail.com>
2022-08-05 12:02:21 +0300

    refactor: тесты для project_services.py

--- server/apps/dcis/tests/services/project_services.py
+++ server/apps/dcis/tests/services/project_services.py
@@ -1,15 +1,17 @@
 """Тесты модуля, отвечающего за работу с проектами."""
-
 from unittest.mock import Mock, patch
 
 from devind_dictionaries.models import Department, Organization
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import PermissionDenied
 from django.test import TestCase
 
 from apps.core.models import User
 from apps.dcis.models import Division, Period, PeriodGroup, PeriodPrivilege, Privilege, Project
+from apps.dcis.permissions.project_permissions import can_add_project, can_change_project, can_delete_project
 from apps.dcis.services.project_services import (
-    get_user_divisions_projects,
+    change_project, create_project,
+    delete_project, get_user_divisions_projects,
     get_user_participant_projects,
     get_user_privileges_projects,
     get_user_projects,
@@ -25,6 +27,7 @@ class GetUserProjectsTestCase(TestCase):
         self.organization_content_type = ContentType.objects.get_for_model(Organization)
 
         self.user = User.objects.create(username='user', email='user@gmain.com')
+        self.super_user = User.objects.create(username='super_user', email='super_user@gmain.com', is_superuser=True)
         self.extra_user = User.objects.create(username='extra_user', email='extra_user@gmail.com')
 
         self.user_projects = [Project.objects.create(
@@ -118,4 +121,70 @@ class GetUserProjectsTestCase(TestCase):
                 self.privilege_project,
             },
             set(get_user_projects(self.user)),
+        )
+
+
+class ProjectsTestCase(TestCase):
+    """Тестирование проекта и прав."""
+    def setUp(self) -> None:
+        """Создание данных для тестирования."""
+        self.organization_content_type = ContentType.objects.get_for_model(Organization)
+
+        self.user = User.objects.create(username='user', email='user@gmain.com')
+        self.super_user = User.objects.create(username='super_user', email='super_user@gmain.com', is_superuser=True)
+        self.extra_user = User.objects.create(username='extra_user', email='extra_user@gmail.com')
+
+        self.data_project = {
+            'name': 'Testing name',
+            'short': 'Testing short',
+            'description': 'Description project',
+            'content_type': self.organization_content_type
+        }
+        self.project = Project.objects.create(
+            name='Testing name 2',
+            short='Testing short 2',
+            description='Description project 2',
+            content_type=ContentType.objects.get_for_model(
+                Project.DIVISION_KIND.get(self.organization_content_type, Department)
+            ),
+            visibility=True,
+            archive=True
+        )
+
+    def test_create_project(self) -> None:
+        """Тестирование функции `create_project`."""
+        with patch.object(self.super_user, 'has_perm', new=lambda perm: perm != 'dcis.add_project'):
+            self.assertRaises(PermissionDenied, can_add_project, self.super_user)
+        self.assertEqual(
+            create_project(self.super_user, self.data_project, True),
+            Project.objects.get(name=self.data_project['name']),
+            'Create project'
+        )
+
+    def test_change_project(self) -> None:
+        """Тестирование функции `change_project`."""
+        with patch.object(self.super_user, 'has_perm', new=lambda perm: perm != 'dcis.change_project'):
+            self.assertRaises(PermissionDenied, can_change_project, self.super_user, self.project)
+        self.assertEqual(
+            change_project(
+                user=self.super_user,
+                project=self.project,
+                name='Change name',
+                short='Change short',
+                description='Change description',
+                visibility=False,
+                archive=False
+            ),
+            Project.objects.get(name='Change name'),
+            'Change project'
+        )
+
+    def test_delete_project(self) -> None:
+        """Тестирование функции `delete_project`."""
+        with patch.object(self.super_user, 'has_perm', new=lambda perm: perm != 'dcis.delete_project'):
+            self.assertRaises(PermissionDenied, can_delete_project, self.super_user, self.project)
+        self.assertEqual(
+            delete_project(user=self.super_user, project=self.project),
+            None,
+            'Delete project'
         )
