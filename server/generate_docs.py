6734ead8180043d72faca377bf5f1243a2a40015
MiloslavskiySergey <sergey07061999@gmail.com>
2022-08-12 16:53:25 +0300

    ci: возврат к старому юзесу

--- server/generate_docs.py
+++ server/generate_docs.py
@@ -19,6 +19,8 @@ def recursive_module(mod: pdoc.doc.Module):
     """Рекурсия прохождения по всем модулям."""
 
 
+
+
     yield mod
     for submod in mod.submodules:
         yield from recursive_module(submod)
