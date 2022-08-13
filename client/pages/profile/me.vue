bfe542b6c79e2c34c9535f7871e42ea69f73fe80
Victor <lyferov@yandex.ru>
2022-08-13 16:28:50 +0300

    feat: Убрано изменение информации, которая забирается с портала

--- client/pages/profile/me.vue
+++ client/pages/profile/me.vue
@@ -1,14 +1,8 @@
 <template lang="pug">
 v-card
   v-card-title {{ $t('profile.me.profile') }} {{ $getUserFullName(user) }}
-  v-card-subtitle {{ $t('profile.me.registrationDate') }} {{ date(user.createdAt) }}
   v-card-text
-    v-row
-      v-col(cols="12" md="3") {{ $t('profile.me.userAvatar') }}
-      v-col(cols="12" md="9")
-        avatar-view(:user="user")
     basic-information(:user="user")
-    additional-information(:viewUser="user")
 </template>
 
 <script lang="ts">
@@ -17,12 +11,10 @@ import { defineComponent, toRef, useNuxt2Meta } from '#app'
 import type { UserType } from '~/types/graphql'
 import { useFilters, useI18n } from '~/composables'
 import { useAuthStore } from '~/stores'
-import AvatarView from '~/components/users/AvatarView.vue'
 import BasicInformation from '~/components/profile/BasicInformation.vue'
-import AdditionalInformation from '~/components/profile/AdditionalInformation.vue'
 
 export default defineComponent({
-  components: { AdditionalInformation, BasicInformation, AvatarView },
+  components: { BasicInformation },
   middleware: 'auth',
   setup () {
     const userStore = useAuthStore()
