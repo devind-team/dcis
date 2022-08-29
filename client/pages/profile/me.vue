7fdb4ed31ffda330ca6ade6fa3ce33a674e711df
Victor <lyferov@yandex.ru>
2022-08-29 12:02:04 +0300

    revert: Добавлены пермишенны на вывод пользовательских настроек

--- client/pages/profile/me.vue
+++ client/pages/profile/me.vue
@@ -1,8 +1,14 @@
 <template lang="pug">
 v-card
   v-card-title {{ $t('profile.me.profile') }} {{ $getUserFullName(user) }}
+  v-card-subtitle {{ $t('profile.me.registrationDate') }} {{ date(user.createdAt) }}
   v-card-text
+    v-row(v-if="hasPerm('core.view_user')")
+      v-col(cols="12" md="3") {{ $t('profile.me.userAvatar') }}
+      v-col(cols="12" md="9")
+        avatar-view(:user="user")
     basic-information(:user="user")
+    additional-information(v-if="hasPerm('core.view_user')" :viewUser="user")
 </template>
 
 <script lang="ts">
@@ -10,11 +16,13 @@ import type { Ref } from '#app'
 import { defineComponent, toRef, useNuxt2Meta } from '#app'
 import type { UserType } from '~/types/graphql'
 import { useFilters, useI18n } from '~/composables'
-import { useAuthStore } from '~/stores'
+import { HasPermissionFnType, useAuthStore } from '~/stores'
+import AvatarView from '~/components/users/AvatarView.vue'
+import AdditionalInformation from '~/components/profile/AdditionalInformation.vue'
 import BasicInformation from '~/components/profile/BasicInformation.vue'
 
 export default defineComponent({
-  components: { BasicInformation },
+  components: { AvatarView, AdditionalInformation, BasicInformation },
   middleware: 'auth',
   setup () {
     const userStore = useAuthStore()
@@ -22,7 +30,8 @@ export default defineComponent({
     const { t } = useI18n()
     useNuxt2Meta({ title: t('profile.me.myProfile') as string })
     const user: Ref<UserType> = toRef(userStore, 'user')
-    return { date, user }
+    const hasPerm: Ref<HasPermissionFnType> = toRef(userStore, 'hasPerm')
+    return { date, user, hasPerm }
   }
 })
 </script>
