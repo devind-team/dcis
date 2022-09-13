2f69dba2df7bd842fbb55fb8243823ef54081e10
Victor <lyferov@yandex.ru>
2022-09-13 17:59:12 +0300

    fix: Исправлен выбор дивизиона

--- client/components/dcis/documents/AddDocument.vue
+++ client/components/dcis/documents/AddDocument.vue
@@ -73,6 +73,7 @@ mutation-modal-form(
         v-list-item-content
           v-list-item-title {{ $t('dcis.documents.addDocument.version', { version: item.version }) }}
           v-list-item-subtitle {{ item.comment }}
+    pre {{ divisionId }}
 </template>
 
 <script lang="ts">
@@ -124,6 +125,9 @@ export default defineComponent({
       if (!props.period.multiple) {
         return null
       }
+      if (props.canAddAnyDocument) {
+        return division.value?.id
+      }
       return props.userDivisions.length === 1 ? props.userDivisions[0].id : division.value?.id
     })
 
@@ -167,6 +171,7 @@ export default defineComponent({
     }
 
     return {
+      divisionId,
       divisionLabel,
       comment,
       status,
