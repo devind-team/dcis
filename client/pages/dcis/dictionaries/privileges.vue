4f8eccdb7aebca9d336530f811d7426ef5a5d835
Roman Ryabtsev <roman@rambler.ru>
2022-09-27 12:32:22 +0300

    feat: Выведена таблица привилегий

---
+++ client/pages/dcis/dictionaries/privileges.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+universal-dictionary(
+  @update-drawer="$emit('update-drawer')"
+  :bread-crumbs="bc"
+  :query="require('~/gql/dcis/queries/privileges.graphql')"
+  :headers="['id', 'name', 'key', 'createdAt']"
+  query-name="privileges"
+)
+  template(#item.createdAt="{ item }")
+</template>
+
+<script lang="ts">
+import type { PropType, ComputedRef } from '#app'
+import { defineComponent, computed, useNuxt2Meta } from '#app'
+import { BreadCrumbsItem } from '~/types/devind'
+import { useFilters, useI18n } from '~/composables'
+import UniversalDictionary from '~/components/dcis/dictionaries/UniversalDictionary.vue'
+export default defineComponent({
+  components: { UniversalDictionary },
+  middleware: ['auth'],
+  props: {
+    breadCrumbs: { type: Array as PropType<BreadCrumbsItem[]>, required: true }
+  },
+  setup (props) {
+    const { t, localePath } = useI18n()
+    useNuxt2Meta({ title: t('dictionaries.privileges.header') as string })
+    const bc: ComputedRef<BreadCrumbsItem[]> = computed<BreadCrumbsItem[]>(() => ([
+      ...props.breadCrumbs,
+      {
+        text: t('dictionaries.privileges.header') as string,
+        to: localePath({ name: 'dcis-dictionaries-privileges' }),
+        exact: true
+      }
+    ]))
+    return { bc, useFilters }
+  }
+})
+</script>
