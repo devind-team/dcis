8e9f241965164457c08d58c921c402334d234158
Svinorez666 <roman623508@rambler.ru>
2022-09-30 16:12:27 +0300

    fix: Добавлена мутация на удаление атрибута периода

--- server/apps/dcis/schema/mutations/attribute_mutations.py
+++ server/apps/dcis/schema/mutations/attribute_mutations.py
@@ -1,7 +1,9 @@
 """Мутации, связанные с атрибутами."""
+from typing import Any
 
 import graphene
 from graphene_django.forms.mutation import DjangoModelFormMutation
+from graphql import ResolveInfo
 from devind_helpers.decorators import permission_classes
 from devind_helpers.orm_utils import get_object_or_404
 from devind_helpers.permissions import IsAuthenticated
@@ -9,6 +11,7 @@ from devind_helpers.permissions import IsAuthenticated
 from apps.dcis.permissions import can_change_period_sheet
 from apps.dcis.models import Attribute, Period
 from apps.dcis.forms import AddAttributeForm, ChangeAttributeForm
+from apps.dcis.services.attribute_service import delete_attribute
 
 
 class AddAttributeMutation(DjangoModelFormMutation):
@@ -37,6 +40,23 @@ class ChangeAttributeMutation(DjangoModelFormMutation):
         attribute: Attribute = get_object_or_404(Attribute, pk=data.get('id'))
         can_change_period_sheet(info.context.user, attribute.period)
         return super().mutate_and_get_payload(root, info, **data)
+
+
+class DeleteAttributeMutation(graphene.ObjectType):
+    """Мутация для удаления атрибута"""
+    class Input:
+        period_id = graphene.ID(required=True, description='Идентификатор периода')
+        attribute_id = graphene.ID(required=True, description='Идентификатор атрибута')
+
+    delete_id = graphene.ID(required=True, description='Идентификатор удаленного атрибута')
+
+    @staticmethod
+    @permission_classes((IsAuthenticated,))
+    def mutate_and_get_payload(root: Any, info: ResolveInfo, period_id: str, attribute_id: str):
+        period = get_object_or_404(Period, pk=period_id)
+        attribute = get_object_or_404(Attribute, pk=attribute_id)
+        delete_attribute(user=info.context.user, period=period, attribute=attribute)
+        return DeleteAttributeMutation(delete_id=attribute_id)
 
 
 class AttributeMutations(graphene.ObjectType):
--- server/apps/dcis/services/attribute_service.py
+++ server/apps/dcis/services/attribute_service.py
@@ -2,7 +2,15 @@
 
 
 from apps.dcis.models import Period, Attribute
+from apps.core.models import User
+from apps.dcis.permissions import can_change_period_attributes
 
 
 def add_attribute(period: Period):
     pass
+
+
+def delete_attribute(user: User, period: Period, attribute: Attribute):
+    """Удаление атрибута."""
+    can_change_period_attributes(user, period)
+    attribute.delete()
