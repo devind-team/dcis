3bc29643921eb605da059edf9f8f0b8340727b54
MiloslavskiySergey <sergey07061999@gmail.com>
2022-10-18 12:52:15 +0300

    feat: добавлены миграции на добавление и удаление пользователей и организации в кураторскую группу

--- server/apps/dcis/schema/mutations/curator_mutation.py
+++ server/apps/dcis/schema/mutations/curator_mutation.py
@@ -7,7 +7,12 @@ from devind_helpers.schema.mutations import BaseMutation
 from graphql import ResolveInfo
 
 from apps.dcis.schema.types import CuratorGroupType
-from apps.dcis.services.curator_services import add_curator_group, delete_curator_group
+from apps.dcis.services.curator_services import (
+    add_curator_group,
+    add_organization_curator_group, add_user_curator_group,
+    delete_curator_group,
+    delete_organization_curator_group, delete_user_curator_group,
+)
 
 
 class AddCuratorGroupMutation(BaseMutation):
@@ -46,8 +51,94 @@ class DeleteCuratorGroupMutation(BaseMutation):
         )
 
 
+class AddUserCuratorGroup(BaseMutation):
+    """Мутация на добавление пользователей в кураторскую группу."""
+
+    class Input:
+        curator_group_id = graphene.ID(required=True, description='Идентификатор кураторской группы')
+        user_id = graphene.ID(required=True, description='Идентификатор пользователя')
+
+    add_user_id = graphene.ID(required=True, description='Идентификатор пользователя')
+
+    @staticmethod
+    # @permission_classes((IsAuthenticated,))
+    def mutate_and_get_payload(root: Any, info: ResolveInfo, curator_group_id: str | int, user_id: str | int):
+        return AddUserCuratorGroup(
+            add_user_id=add_user_curator_group(
+                curator_group_id=curator_group_id,
+                user_id=user_id
+            )
+        )
+
+
+class DeleteUserCuratorGroup(BaseMutation):
+    """Мутация на добавление пользователей в кураторскую группу."""
+
+    class Input:
+        curator_group_id = graphene.ID(required=True, description='Идентификатор кураторской группы')
+        user_id = graphene.ID(required=True, description='Идентификатор пользователя')
+
+    delete_user_id = graphene.ID(required=True, description='Идентификатор пользователя')
+
+    @staticmethod
+    # @permission_classes((IsAuthenticated,))
+    def mutate_and_get_payload(root: Any, info: ResolveInfo, curator_group_id: str | int, user_id: str | int):
+        return AddUserCuratorGroup(
+            delete_user_id=delete_user_curator_group(
+                curator_group_id=curator_group_id,
+                user_id=user_id
+            )
+        )
+
+
+class AddOrganizationCuratorGroup(BaseMutation):
+    """Мутация на добавление организации в кураторскую группу."""
+
+    class Input:
+        curator_group_id = graphene.ID(required=True, description='Идентификатор кураторской группы')
+        organization_id = graphene.ID(required=True, description='Идентификатор организации')
+
+    add_organization_id = graphene.ID(required=True, description='Идентификатор организации')
+
+    @staticmethod
+    # @permission_classes((IsAuthenticated,))
+    def mutate_and_get_payload(root: Any, info: ResolveInfo, curator_group_id: str | int, organization_id: str | int):
+        return AddOrganizationCuratorGroup(
+            add_user_id=add_organization_curator_group(
+                curator_group_id=curator_group_id,
+                organization_id=organization_id
+            )
+        )
+
+
+class DeleteUOrganizationCuratorGroup(BaseMutation):
+    """Мутация на добавление пользователей в кураторскую группу."""
+
+    class Input:
+        curator_group_id = graphene.ID(required=True, description='Идентификатор кураторской группы')
+        organization_id = graphene.ID(required=True, description='Идентификатор организации')
+
+    delete_organization_id = graphene.ID(required=True, description='Идентификатор организации')
+
+    @staticmethod
+    # @permission_classes((IsAuthenticated,))
+    def mutate_and_get_payload(root: Any, info: ResolveInfo, curator_group_id: str | int, organization_id: str | int):
+        return AddUserCuratorGroup(
+            delete_user_id=delete_organization_curator_group(
+                curator_group_id=curator_group_id,
+                organization_id=organization_id
+            )
+        )
+
+
 class CuratorMutations(graphene.ObjectType):
     """Список мутаций кураторов."""
 
     add_curator_group = AddCuratorGroupMutation.Field(required=True)
     delete_curator_group = DeleteCuratorGroupMutation.Field(required=True)
+
+    add_user_curator_group = AddUserCuratorGroup.Field(required=True)
+    delete_user_curator_group = DeleteUserCuratorGroup.Field(required=True)
+
+    add_organization_curator_group = AddOrganizationCuratorGroup.Field(required=True)
+    delete_organization_curator_group = DeleteUOrganizationCuratorGroup.Field(required=True)
--- server/apps/dcis/services/curator_services.py
+++ server/apps/dcis/services/curator_services.py
@@ -1,14 +1,43 @@
-from apps.dcis.models import CuratorGroup
 from devind_helpers.orm_utils import get_object_or_404
 
+from apps.dcis.models import CuratorGroup
 
 
 def add_curator_group(name: str) -> CuratorGroup:
     """Добавление кураторской группы."""
     return CuratorGroup.objects.create(name=name)
 
+
 def delete_curator_group(curator_group_id: str | int) -> str | int:
     """Удаление кураторской группы."""
     curator_group: CuratorGroup = get_object_or_404(CuratorGroup, pk=curator_group_id)
     curator_group.delete()
     return curator_group_id
+
+
+def add_user_curator_group(curator_group_id: str | int, user_id: str | int) -> str | int:
+    """Добавление пользователя в кураторскую группу."""
+    curator_group: CuratorGroup = get_object_or_404(CuratorGroup, pk=curator_group_id)
+    curator_group.users.add(user_id)
+    return user_id
+
+
+def delete_user_curator_group(curator_group_id: str | int, user_id: str | int) -> str | int:
+    """Добавление пользователя в кураторскую группу."""
+    curator_group: CuratorGroup = get_object_or_404(CuratorGroup, pk=curator_group_id)
+    curator_group.users.remove(user_id)
+    return user_id
+
+
+def add_organization_curator_group(curator_group_id: str | int, organization_id: str | int) -> str | int:
+    """Добавление пользователя в кураторскую группу."""
+    curator_group: CuratorGroup = get_object_or_404(CuratorGroup, pk=curator_group_id)
+    curator_group.organization.add(organization_id)
+    return organization_id
+
+
+def delete_organization_curator_group(curator_group_id: str | int, organization_id: str | int) -> str | int:
+    """Добавление пользователя в кураторскую группу."""
+    curator_group: CuratorGroup = get_object_or_404(CuratorGroup, pk=curator_group_id)
+    curator_group.organization.remove(organization_id)
+    return organization_id
