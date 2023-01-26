ed420bb7f1f8c80820683b784bde9ed947a87364
Roman Ryabtsev <roman@rambler.ru>
2023-01-26 16:37:04 +0300

    ref: Добавлено поле "Тип сообщения" в комментарии документа

--- server/apps/dcis/models/messages.py
+++ server/apps/dcis/models/messages.py
@@ -7,7 +7,16 @@ from .document import Document
 class DocumentMessage(models.Model):
     """Модель комментариев к документу"""
 
+    MESSAGE = 'message'
+    STATUS = 'status'
+
+    KIND_DOCUMENT_MESSAGE = (
+        (MESSAGE, 'message'),
+        (STATUS, 'status')
+    )
+
     comment = models.TextField(max_length=1023, help_text='Комментарий')
+    kind = models.CharField(max_length=10, default=MESSAGE, choices=KIND_DOCUMENT_MESSAGE, help_text='Тип сообщения')
     created_at = models.DateTimeField(auto_now_add=True, help_text='Дата создания')
     user = models.ForeignKey(
         User,
