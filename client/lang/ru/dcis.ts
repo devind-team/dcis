640d66a69078cdd858bf5b2f6f95ada960ff8fe1
Roman Ryabtsev <roman@rambler.ru>
2023-02-03 15:09:09 +0300

    ref: Переименован раздел "Комментарии" в "Журнал"

--- client/lang/ru/dcis.ts
+++ client/lang/ru/dcis.ts
@@ -305,7 +305,7 @@ export default {
     links: {
       sheets: 'Листы',
       attributes: 'Атрибуты',
-      comments: 'Комментарии'
+      comments: 'Журнал'
     },
     tableHeaders: {
       version: 'Версия',
