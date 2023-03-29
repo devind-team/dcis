f8193286f69f5014a4cdbea63086acdcea92d3ba
Roman Ryabtsev <roman@rambler.ru>
2023-03-29 13:16:18 +0300

    feat: подготовка страницы методического обеспечения

--- client/pages/dcis/periods/_periodId/support.vue
+++ client/pages/dcis/periods/_periodId/support.vue
@@ -1,12 +1,32 @@
 <template lang="pug">
-  pre {{ 'Hallo' }}
+left-navigator-container(:bread-crumbs="bc" @update-drawer="$emit('update-drawer')")
+  template(#header) {{ $t('dcis.periods.links.support') }}
 </template>
 <script lang="ts">
-export default {
-  name: 'Support'
-}
+import { defineComponent, computed, ComputedRef, PropType } from '#app'
+import { BreadCrumbsItem } from '~/types/devind'
+import { useI18n } from '~/composables'
+import LeftNavigatorContainer from '~/components/common/grid/LeftNavigatorContainer.vue'
+
+export default defineComponent({
+  components: { LeftNavigatorContainer },
+  middleware: 'auth',
+  props: {
+    breadCrumbs: { type: Array as PropType<BreadCrumbsItem[]>, required: true }
+  },
+  setup (props) {
+    const { t, localePath } = useI18n()
+    const bc: ComputedRef<BreadCrumbsItem[]> = computed<BreadCrumbsItem[]>(() => ([
+      ...props.breadCrumbs,
+      {
+        text: t('dcis.periods.links.support') as string,
+        to: localePath({ name: 'dcis-periods-periodId-support' }),
+        exact: true
+      }
+    ]))
+    return {
+      bc
+    }
+  }
+})
 </script>
-
-<style scoped>
-
-</style>
