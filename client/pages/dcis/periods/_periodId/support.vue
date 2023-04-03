7158a996f70866d7541c4c7cc5aabf1d8ad06acb
Roman Ryabtsev <roman@rambler.ru>
2023-04-03 13:56:24 +0300

    feat: улучшен вывод методических рекомендаций периода

--- client/pages/dcis/periods/_periodId/support.vue
+++ client/pages/dcis/periods/_periodId/support.vue
@@ -20,6 +20,34 @@ left-navigator-container(:bread-crumbs="bc" @update-drawer="$emit('update-drawer
           disable-pagination
           hide-default-footer
         )
+          template(#item.name="{ item }")
+            a(:href="`/${item.src}`" target="__blank") {{ item.name }}
+          template(#item.updated="{ item }") {{ $filters.dateTimeHM(item.updatedAt) }}
+          template(#item.size="{ item }") {{ (item.size / 1024).toFixed(2) }} {{ $t('profile.files.kB') }}
+          template(#item.actions="{ item }")
+            text-menu(v-slot="{ on: onMenu }" :value="item.name" @update="changeFileMutate({ fileId: item.id, field: 'name', value: $event }).then()")
+              v-tooltip(bottom)
+                template(#activator="{ on: onTooltipEdit }")
+                  v-btn(v-on="{...onMenu, ...onTooltipEdit}" icon color="success")
+                    v-icon mdi-pencil
+                span {{ $t('profile.files.changeName') }}
+            v-btn(v-if="item.deleted" @click="changeFileMutate({ fileId: item.id, field: 'deleted', value: 'false' }).then()" icon color="warning")
+              v-icon mdi-delete-restore
+            v-tooltip(v-else bottom)
+              template(#activator="{ on: onTooltip }")
+                v-menu(bottom)
+                  template(#activator="{ on: onMenu }")
+                    v-btn(v-on="{...onTooltip, ...onMenu}" icon color="red")
+                      v-icon mdi-delete
+                  v-card
+                    v-card-text {{ $t('profile.files.deletingFile') }}
+                    v-card-actions
+                      v-btn(
+                        v-if="hasPerm('devind_core.delete_file')"
+                        @click="deleteFileMutate({ fileId: item.id }).then()" color="error"
+                      ) {{ $t('profile.files.delete') }}
+                      v-btn(@click="changeFileMutate({ fileId: item.id, field: 'deleted', value: 'true' }).then()" color="warning") {{ $t('profile.files.delete') }}
+              span {{ $t('profile.files.deleteFile') }}
   pre {{ files }}
 </template>
 <script lang="ts">
@@ -30,7 +58,12 @@ import { BreadCrumbsItem } from '~/types/devind'
 import { useCursorPagination, useDebounceSearch, useI18n, useQueryRelay, useSelectFiles } from '~/composables'
 import LeftNavigatorContainer from '~/components/common/grid/LeftNavigatorContainer.vue'
 import {
-  AddFileMutation, AddFileMutationVariables,
+  AddFileMutation,
+  AddFileMutationVariables,
+  ChangeFileMutation,
+  ChangeFileMutationVariables,
+  DeleteFileMutation,
+  DeleteFileMutationVariables,
   MethodicalSupportQuery,
   MethodicalSupportQueryVariables,
   PeriodType,
@@ -39,9 +72,12 @@ import {
 import periodMethodicalSupportQuery from '~/gql/dcis/queries/period_methodical_support.graphql'
 import { HasPermissionFnType, useAuthStore } from '~/stores'
 import addFile from '~/gql/core/mutations/file/add_file.graphql'
+import TextMenu from '~/components/common/menu/TextMenu.vue'
+import deleteFile from '~/gql/core/mutations/file/delete_file.graphql'
+import changeFile from '~/gql/core/mutations/file/change_file.graphql'
 
 export default defineComponent({
-  components: { LeftNavigatorContainer },
+  components: { TextMenu, LeftNavigatorContainer },
   middleware: 'auth',
   props: {
     breadCrumbs: { type: Array as PropType<BreadCrumbsItem[]>, required: true },
@@ -50,7 +86,7 @@ export default defineComponent({
   setup (props) {
     const { t, localePath } = useI18n()
     const authStore = useAuthStore()
-    const { user } = toRefs<{ user: UserType, hasPerm: HasPermissionFnType }>(authStore)
+    const { user, hasPerm } = toRefs<{ user: UserType, hasPerm: HasPermissionFnType }>(authStore)
 
     const { search, debounceSearch } = useDebounceSearch({
       callback: () => setPage(1)
@@ -91,6 +127,12 @@ export default defineComponent({
     const { select: addFilesHandle } = useSelectFiles((files: FileList) => {
       addFileMutate({ userId: user.value.id, files })
     })
+    const { mutate: changeFileMutate } = useMutation<ChangeFileMutation, ChangeFileMutationVariables>(changeFile, {
+      update: (cache, result) => changeUpdate(cache, result, 'file')
+    })
+    const { mutate: deleteFileMutate } = useMutation<DeleteFileMutation, DeleteFileMutationVariables>(deleteFile, {
+      update: (cache, result) => deleteUpdate(cache, result)
+    })
     const bc: ComputedRef<BreadCrumbsItem[]> = computed<BreadCrumbsItem[]>(() => ([
       ...props.breadCrumbs,
       {
@@ -101,7 +143,7 @@ export default defineComponent({
     ]))
     return {
       bc,
-      // hasPerm,
+      hasPerm,
       headers,
       search,
       debounceSearch,
@@ -112,9 +154,9 @@ export default defineComponent({
       fetchMoreAvailable,
       fetchMoreData,
       setPage,
-      addFilesHandle
-      // changeFileMutate,
-      // deleteFileMutate
+      addFilesHandle,
+      changeFileMutate,
+      deleteFileMutate
     }
   }
 })
