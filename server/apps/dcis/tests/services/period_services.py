03686f7509767e1354aa6b74951b3b3b09f41039
squak <squakrazv@gmail.com>
2023-04-17 17:08:40 +0300

    test: исправлен тест создания периода

--- server/apps/dcis/tests/services/period_services.py
+++ server/apps/dcis/tests/services/period_services.py
@@ -36,6 +36,7 @@ from apps.dcis.services.period_services import (
     get_user_periods,
     get_user_privileges_periods,
 )
+from apps.dcis.tests.tests_helpers import create_in_memory_file
 
 
 class GetUserPeriodsTestCase(TestCase):
@@ -238,6 +239,8 @@ class PeriodTestCase(TestCase):
             project=self.project,
             multiple=True,
             versioning=True,
+            readonly_fill_color=False,
+            xlsx_file=create_in_memory_file('test_create_period.xlsx'),
             limitations_file=None,
         )
 
